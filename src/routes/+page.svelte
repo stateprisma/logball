70b66c3bfe66ff86c7bfbc2e3b30accea2c5485d
Wrench56 <dmarkreg@gmail.com>
2024-09-14 00:44:36 -0400

    feat: basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,32 @@
-<h1>Hello World</h1>
+<script lang="ts">
+	import UartConsole from '$lib/UartConsole.svelte';
+	import Header from './Header.svelte';
+</script>
+
+<Header />
+
+<div class="layout">
+	<div class="left"></div>
+
+	<div class="right">
+		<UartConsole />
+	</div>
+</div>
+
+<style>
+	.layout {
+		display: flex;
+		width: 100vw;
+		height: 100vh;
+	}
+
+	.left {
+		width: 50%;
+		height: 100%;
+	}
+
+	.right {
+		width: 50%;
+		height: 100%;
+	}
+</style>

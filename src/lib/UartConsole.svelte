f3a7ab4907ade35dfae65f2ebf976acfd39229a2
xadaemon <soft.song@fastmail.net>
2024-09-15 03:27:59 +0200

    refactor(all): refactor to use bootstrap to avoid hand written CSS

--- src/lib/UartConsole.svelte
+++ src/lib/UartConsole.svelte
@@ -11,6 +11,23 @@
 	import { UART_HEIGHT, UART_WIDTH } from './constants';
 	import { calculateRows } from './utils';
 	import { VERSION } from './constants';
+	import {
+		Col,
+		Container,
+		Form,
+		FormGroup,
+		Input,
+		Row,
+		Button,
+		Icon,
+		Card,
+		CardBody,
+		CardFooter,
+		CardHeader,
+		CardSubtitle,
+		CardText,
+		CardTitle
+	} from '@sveltestrap/sveltestrap';
 
 	let options: ITerminalOptions & ITerminalInitOnlyOptions = {
 		fontSize: 15,
@@ -22,13 +39,14 @@
 	let socket: WebSocket;
 	let fitAddon: FitAddon;
 	let terminal: Terminal;
-	let statusIcon: string;
+	let statusColor: string;
 	let statusText: string;
 	let uartAddress: string = 'ws://localhost:1234';
 	let container: HTMLDivElement;
 
 	async function onLoad(event: CustomEvent<{ terminal: Terminal }>) {
 		terminal = event.detail.terminal;
+		statusColor = 'red';
 
 		/* FitAddon Usage */
 		fitAddon = new (await XtermAddon.FitAddon()).FitAddon();
@@ -46,7 +64,7 @@
 
 		statusText = 'Idle';
 		terminal.write(`> Logball v${VERSION}\r\n`);
-		terminal.write('=================\r\n')
+		terminal.write('=================\r\n');
 	}
 
 	async function setupWebSocket() {
@@ -102,92 +120,31 @@
 	});
 </script>
 
-<div bind:this={container} id="terminal-container">
-	<Xterm class="xterm" {options} on:load={onLoad} on:key={onKey} on:data={onData} />
-</div>
-<div class="container">
-	<form id="controls">
-		<input class="input-field" type="text" bind:value={uartAddress} />
-		<input class="button" type="button" value="Connect" on:click={setupWebSocket} />
-		<p class="status-text">Status: {statusText}</p>
-	</form>
-</div>
-
-<style>
-	#terminal-container {
-		position: relative;
-		right: 0;
-		top: 20px;
-		bottom: 20px;
-		left: 20px;
-		width: calc(100% - 40px);
-		height: calc(100% - 100px);
-		background-color: black;
-		padding: 10px;
-		box-sizing: border-box;
-		overflow: hidden;
-		border-radius: 12px;
-	}
-
-	#controls {
-		position: relative;
-		margin: 35px;
-	}
-
-	.container {
-		position: relative;
-		display: inline-flex;
-		background-color: rgba(26, 30, 38, 1);
-		z-index: 2;
-		border-radius: 12px;
-		padding: 4px;
-		height: 6%;
-		align-items: center;
-		top: 25px;
-		bottom: 20px;
-		right: 25px;
-		left: 20px;
-		min-height: 50px !important;
-		width: calc(100% - 48px);
-		box-shadow:
-			inset 4px 4px 8px 0 rgba(0, 0, 0, 0.9),
-			inset -4px -4px 8px 0 rgba(40, 40, 40, 0.9);
-	}
-
-	.button {
-		display: inline-block;
-		padding: 10px 24px;
-		background-color: #1688e6;
-		color: var(--color-text);
-		font-size: 16px;
-		font-weight: bold;
-		border: none;
-		border-radius: 8px;
-		cursor: pointer;
-		transition:
-			background-color 0.2s ease,
-			box-shadow 0.2s ease;
-	}
-
-	.button:hover {
-		background-color: #006bc3;
-	}
-
-	.input-field {
-		position: relative;
-		display: inline-block;
-		left: 0px;
-		padding: 8px;
-		margin-right: 10px;
-		color: var(--color-text);
-		background-color: var(--color-bg-1);
-		outline: none;
-		border: 2px solid whitesmoke;
-		border-radius: 8px;
-	}
-
-	.status-text {
-		display: inline-block;
-		margin-left: 20px;
-	}
-</style>
+<Card>
+	<CardHeader>
+		<CardTitle>Live UART</CardTitle>
+	</CardHeader>
+	<CardBody>
+		<Container fluid class="grid">
+			<Row>
+				<Col>
+					<Xterm class="xterm" {options} on:load={onLoad} on:key={onKey} on:data={onData} />
+				</Col>
+			</Row>
+			<Row class="mt-2"></Row>
+		</Container>
+	</CardBody>
+	<CardFooter>
+		<Form>
+			<Row>
+				<Col>
+					<label for="uart-addr">Uart Address</label>
+					<Input bind:value={uartAddress} name="uart-addr" />
+				</Col>
+				<Col class='mt-4'>
+					<Button type="button" on:click={setupWebSocket}>Connect</Button>
+				</Col>
+			</Row>
+		</Form>
+	</CardFooter>
+</Card>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
 	import UartConsole from '$lib/UartConsole.svelte';
 	import Header from './Header.svelte';
+	import { Styles, Row, Col, Container } from '@sveltestrap/sveltestrap';
+
+	// ts complains unecessarilly here
+	let theme: any = 'auto';
 </script>
 
+<Styles {theme} />
 <Header />
-
-<div class="layout">
-	<div class="left"></div>
-
-	<div class="right">
-		<UartConsole />
-	</div>
-</div>
-
-<style>
-	.layout {
-		display: flex;
-		width: 100vw;
-		height: 100vh;
-	}
-
-	.left {
-		width: 50%;
-		height: 100%;
-	}
-
-	.right {
-		width: 50%;
-		height: 100%;
-	}
-</style>
+<Container fluid>
+	<Row>
+		<Col>
+			<p>
+				Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent porttitor habitasse vestibulum
+				turpis magnis augue sagittis augue. Aptent turpis montes dignissim; amet nec quam ultrices.
+				Sed consectetur ipsum phasellus orci class primis. Turpis urna nisi ante tempor pharetra
+				vitae pretium mollis iaculis. Integer felis sodales mauris dignissim platea sit. Volutpat
+				maximus fringilla euismod ex ultricies dui. Primis bibendum montes turpis arcu condimentum
+				nec cras orci. Sociosqu scelerisque vel tortor semper ante. Aptent facilisis dapibus platea
+				platea augue conubia taciti. Etiam dapibus nisl est ultrices nam vestibulum netus augue.
+				Nunc massa libero lacinia quam diam lacinia aptent. Eu finibus gravida lorem magnis dapibus
+				vulputate. Lacus rhoncus curae dictum mattis porttitor commodo sociosqu ante. Porttitor
+				platea sollicitudin mattis parturient curabitur.
+			</p>
+		</Col>
+		<Col>
+			<UartConsole />
+		</Col>
+	</Row>
+</Container>

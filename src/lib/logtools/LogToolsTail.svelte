104d65fad0f815d328f18790caa20a8fba16ff54
Wrench56 <markdevenyidev@gmail.com>
2024-09-28 22:46:34 -0400

    feat(logtools): display filtered and formatted logs on "Log Tail" tab

--- src/lib/logtools/LogToolsTail.svelte
+++ src/lib/logtools/LogToolsTail.svelte
@@ -1,7 +1,22 @@
 <script>
-	import { TabPane } from '@sveltestrap/sveltestrap';
+	import { logStore } from '$lib/stores/logs';
+	import { TabPane, ListGroup } from '@sveltestrap/sveltestrap';
+	import { formatLog } from './logutils';
 </script>
 
 <TabPane tabId="tail" tab="Log Tail" active>
-	<p>Under construction</p>
+	<ListGroup>
+		{#each $logStore as log}
+			<p class="log-entry">{@html formatLog(log)}</p>
+		{/each}
+	</ListGroup>
 </TabPane>
+
+<style>
+	.log-entry {
+		white-space: pre;
+		margin: 0px;
+		margin-bottom: -5px;
+		padding: 0px;
+	}
+</style>
